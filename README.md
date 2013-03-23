75e3b4212fe1b65b19c157ca2e18e273d6b112be
Kyle Stevens <kstevens715@gmail.com>
2013-03-23 00:38:28 -0400

    Working as a jQuery plugin.

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 ddTables
 ========
 
-A JavaScript widget that allows dragging and dropping data from desktop spreadsheet applications. 
+A jQuery plugin that allows dragging and dropping data from desktop spreadsheet applications. 
 You can then define a callback function to arbitrarily process each record.
+
+* Have a shell of a jQuery plugin.
+* Have basic testing framework in place.
+* Have drag and drop functionality creating data structure.
+* It should also create an HTML table.
+
+Example
+=======
+
+$('#spreadsheet').dropify({
+  fields: { "field1": true, "field2": false }
+  processRecords: function(record) {
+    someAjaxCall(record.field1, record.field2);
+  }
+});
+
---
+++ ddtables.js
@@ -0,0 +1,76 @@
+(function( $ ) {
+
+  $.fn.ddTable = function() {
+
+    this.on('dragover', dragOver);
+    this.on('drop', dragDrop);
+    this.on('dragenter', dragEnter);
+    this.on('dragleave', dragLeave);
+  };
+
+  function dragDrop(e) {
+    //text/plain
+    var src = e.originalEvent.dataTransfer.getData("text/html");
+
+    c = $('<div></div>')
+    c.html(src)
+    $(this).html('<table>' + $('table', c).html() + '</table>')
+
+    console.log(src);
+    return true;
+  };
+
+  function dragOver(e) {
+    // return false to allow drops, true otherwise.
+    return false;
+  };
+
+  function dragEnter(e) {
+    //todo: addClass
+    return false;
+  };
+
+  function dragLeave(e) {
+    //todo: removeClass
+    return false;
+  };
+
+})( jQuery );
+
+//   // $('#droppablediv').dropImport(
+//   //  fields: { one: true, two: false}
+//   //  processRecords: function(record) { }
+// function processRecords(fn) {
+//   // for each record
+//   // record = { colname: value, col2name: value }
+//   // fn(record)
+// }
+// 
+// function parseData(data) {
+//   var lines = [];
+//   var rows = data.split("\n");
+//   for (var rowIndex=0; rowIndex<rows.length; rowIndex++) {
+//     row = rows[rowIndex].split("\t");
+//     lines[rowIndex] = []
+//     for (var colIndex=0; colIndex<row.length; colIndex++) {
+//       lines[rowIndex][colIndex] = row[colIndex]
+//     }
+//   }
+//   return lines
+// }
+// 
+// function drawHtml(rows) {
+//   sheet = document.getElementById('spreadsheet');
+//   output = "<table>";
+//   for (var rowIndex=0; rowIndex<rows.length; rowIndex++) {
+//     row = rows[rowIndex];
+//     output += "<tr>"
+//     for (var colIndex=0; colIndex<row.length; colIndex++) {
+//       output = output + "<td>" + row[colIndex] + "</td>";
+//     }
+//     output += "</tr>";
+//   }
+//   output += "</table>";
+//   sheet.innerHTML = output;
+// }
+// 
